4884bf74d41385e271acd679b7753bc14991da48
Maxx Zhao <maxx@Maxxs-MacBook-Pro.local>
2023-02-01 18:14:30 -0800

    gps finds target bugfix

--- src/chassis.cpp
+++ src/chassis.cpp
@@ -243,8 +243,8 @@ void seek(int xCord, int yCord, int time) {
 
     double dist_Error;
     double dist_PidSpeed, dist_Derivitive, dist_TotalError, dist_PreviousError = 0.0;
-    float dist_P = 0.5;
-    float dist_I = 0.0;
+    float dist_P = 0.05;
+    float dist_I = 0.003333333;
     float dist_D = 0.0;
 
     std::vector<float> dampenerH;
@@ -268,8 +268,37 @@ void seek(int xCord, int yCord, int time) {
         float dampedHeading = std::accumulate(dampenerH.begin(), dampenerH.end(), 0.0) / dampenerH.size();
         
         float radiansToTarget = atan(dampedDeltaY / dampedDeltaX);
-        float degreesToTarget = (radiansToTarget / 3.141592) * 180.0;
-        float headingToTarget = 90.0 - degreesToTarget;
+        float degreesToTarget = fabs((radiansToTarget / 3.141592) * 180.0);
+        float headingToTarget;
+        
+        if (true) { // align gps frontside to target
+            if (dampedDeltaX <= 0 && dampedDeltaY <= 0) {
+                headingToTarget = -(90 + degreesToTarget);
+            }
+            else if (dampedDeltaX >= 0 && dampedDeltaY >= 0) {
+                headingToTarget = (90 - degreesToTarget);
+            }
+            else if (dampedDeltaX >= 0 && dampedDeltaY <= 0) {
+                headingToTarget = (90 + degreesToTarget);
+            }
+            else if (dampedDeltaX <= 0 && dampedDeltaY >= 0) {
+                headingToTarget = -(90 - degreesToTarget);
+            }
+        }
+        else if (false) { // align gps backside to target
+            if (dampedDeltaX >= 0 && dampedDeltaY >= 0) {
+                headingToTarget = -(90 + degreesToTarget);
+            }
+            else if (dampedDeltaX <= 0 && dampedDeltaY <= 0) {
+                headingToTarget = (90 - degreesToTarget);
+            }
+            else if (dampedDeltaX <= 0 && dampedDeltaY >= 0) {
+                headingToTarget = (90 + degreesToTarget);
+            }
+            else if (dampedDeltaX >= 0 && dampedDeltaY <= 0) {
+                headingToTarget = -(90 - degreesToTarget);
+            }
+        }
 
         turn_Error = headingToTarget - dampedHeading;
         turn_TotalError += turn_Error * 0.02;
@@ -299,6 +328,8 @@ void seek(int xCord, int yCord, int time) {
 
     //     std::cout << turn_PidSpeed << " " << dist_PidSpeed << std::endl;
 
+        dist_PidSpeed = 0;
+
         frontRightDriveMotor.move(turn_PidSpeed + dist_PidSpeed);
         frontLeftDriveMotor.move(-turn_PidSpeed + dist_PidSpeed);
         backRightDriveMotor.move(turn_PidSpeed + dist_PidSpeed);
@@ -307,7 +338,6 @@ void seek(int xCord, int yCord, int time) {
         pros::delay(20);
     }
 }
-
 
 // functions below this line suffer from sensor error too much to be viable at this time
 
