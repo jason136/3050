e7d836d70bbb8b220c56e7a7349916b93ae44f0b
gleila123 <leilagheysar@yahoo.com>
2023-03-05 14:05:27 -0800

    Blue2 Auton

--- src/auton.cpp
+++ src/auton.cpp
@@ -259,7 +259,7 @@ void autoRed2() {
 	spinFlywheel(true);
     pros::delay(200);
 
-    gyroTurn(-92, 1500);
+    gyroTurn(92, 1500);
 
     //Fire disc 1
     setIndexer(1);
@@ -288,7 +288,80 @@ void autoRed2() {
 }
 
 void autoBlue2() {
+  resetGyro();
+    setIndexer(0);
 
+    // drive to first roller
+    driveForDistancePID(-11, 150);
+    gyroTurn(90, 1000);
+
+	setFlywheelSpeed(505);
+	spinFlywheel(true);
+
+    // spin first roller
+    driveForDistancePID(-2, 150);
+    spinIntake(-80);
+    chassisMoveIndividuals(-40, -40, -40, -40);
+    pros::delay(500);
+    spinIntake(0);
+    driveForDistancePID(2, 150);
+
+    gyroTurn(7, 1500);
+
+    // fire 2 disks
+    // Disc 1
+    setIndexer(1);
+    pros::delay(200);
+    setIndexer(0);
+
+	setFlywheelSpeed(510);
+    pros::delay(2000);
+
+   //Disc 2
+    setIndexer(1);
+    pros::delay(200);
+    setIndexer(0);
+
+    // Stop spinning flywheel
+	spinFlywheel(false);
+
+    //Turn; recollect discs
+    gyroTurn(125, 1500);
+
+    //Disc collection
+    spinIntake(127);
+
+    // Drive to center
+    driveForDistancePID(-40, 75);
+
+	setFlywheelSpeed(470);
+	spinFlywheel(true);
+    pros::delay(200);
+
+    gyroTurn(-92, 1500);
+
+    //Fire disc 1
+    setIndexer(1);
+    pros::delay(200);
+    setIndexer(0);
+
+    pros::delay(1000);
+
+    //Fire disc 2
+    setIndexer(1);
+    pros::delay(200);
+    setIndexer(0);
+
+    pros::delay(1000);
+
+    //Fire disc 3
+    setIndexer(1);
+    pros::delay(200);
+    setIndexer(0);
+
+    //Stop spinning flywheel
+    spinIntake(0);
+    spinFlywheel(false);
 }
 
 void autoRed3() {
